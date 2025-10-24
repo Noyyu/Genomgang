a58558bf0790ae564e72cb93acc50d84df3cf591
Bellanthie <bellanthie@gmail.com>
2025-10-24 15:30:10 +0200

    lärt mig om att en metod är ett eget rum. gjort ett matte rum och gått in på hur man lär att anropa och /använda en metod någon annan stans...typ..

--- Program.cs
+++ Program.cs
@@ -2,9 +2,61 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //det är på scenen ALLT "HÄNDER"
         {
-            Console.WriteLine("Hello, World!");
+
+            int x = 7;
+            int y= 17;
+            int z = Mathroom(x,y);
+
+
+            int glassAmount = Glas()
+
+
+            string name1 = "pelle";
+            string name2 = "nikki";
+
+            Welcome(name1);
+
+
+
         }
+
+        // "VOID" är som ett soprum: du ger info= sopor till soprummet. Den tackar dig och du GET THE FUCK OUT. finished!
+        static void Welcome(string name) // void visar bara den returnerar inget värde. 
+        {
+            Console.WriteLine($"Welcome {name} ");
+        }
+
+
+        //void är: vad kan hända på scen utan att visas på scen? Denna returnerar ingenting
+        static int Mathroom(int p, int ö) //detta rummet (metoden) behöver eget papper/egna resurser för uträkning:pappper/penna/calculator
+        {
+            // regardless of using int/bool/string/etc variables--> its enough to declare them within the mehtod room ir "Mathroom" method.
+            //the 'int' below is the 'order' 
+            int z = p + ö;
+            return z;
+        }
+
+        static int Glas()
+        {
+            int basket1 = 5;
+            int basket2 = 5;
+            int glasses = 0;
+
+
+            //basket1 + basket2
+            // vi skapade 'glasses' för att spara RESULTATET på beräkningen för 'basket1' och 'basket2'.
+            // och som en SHORTCUT använder vi/ropar vi på 'Mathroom' och säger att addera basket1 och basket2
+            // och RETURN = GLASSES -> ger tillbaka svaret (som en liten flyttbar calculator-ish)
+            glasses = Mathroom(basket1 , basket2);
+            return glasses;
+
+
+        }
+
+
+
+
     }
 }
