fa34b479d7178e2e7c4e7e4e4cf07c38145efa90
Noyyu <Nikki.Norberg@hotmail.com>
2025-10-24 15:29:07 +0200

    lesson 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int bob = 1;
+            int linus = 2;
+            int z = MathRoom(bob, linus);
+            string name1 = "Roger";
+            string name2 = "Bella";
+            string name3 = "Pelle";
+            string namn4 = "Mjölk";
+
+            Welcome(name1);
+            Welcome(name2);
+            Welcome(name3);
+            Welcome(namn4);
+
+
+            z = bob + linus;
+        }
+
+        static void Welcome(string namn)
+        {
+            Console.WriteLine("----------");
+            Console.WriteLine("----------");
+            Console.WriteLine(namn);
+            Console.WriteLine("----------");
+            Console.WriteLine("----------");
+        }
+        static int MathRoom(int kuk, int röv)
+        {
+            return kuk + röv;
         }
     }
 }
